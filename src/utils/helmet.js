4c425bd0c7680b6324ab0214e777c3232f6097ba
Baldur Helgason <baldur.helgason@gmail.com>
2016-11-23 08:48:55 +0000

    Updates webpack and webpack-dev-server
    and adds `-loader` postfixes where needed

--- src/utils/helmet.js
+++ src/utils/helmet.js
@@ -13,16 +13,16 @@ export default {
     content: '#aa0078',
   }, {
     name: 'msapplication-TileImage',
-    content: require('!file!assets/favicon/mstile-150x150.png'),
+    content: require('!file-loader!assets/favicon/mstile-150x150.png'),
   }, {
     name: 'theme-color',
     content: '#ffffff',
   }, {
     property: 'og:image',
-    content: require('!file!assets/favicon/android-chrome-192x192.png'),
+    content: require('!file-loader!assets/favicon/android-chrome-192x192.png'),
   },
   { name: 'twitter:card', content: 'summary_large_image' },
-  { name: 'twitter:image', content: require('!file!assets/favicon/android-chrome-192x192.png') },
+  { name: 'twitter:image', content: require('!file-loader!assets/favicon/android-chrome-192x192.png') },
   { name: 'twitter:site', content: '@ueno' },
   { name: 'twitter:creator', content: '@ueno' },
   { name: 'twitter:description', content: 'Ueno. description text here!' },
@@ -30,24 +30,24 @@ export default {
   ],
   link: [{
     rel: 'manifest',
-    href: require('!file!assets/favicon/manifest.json'),
+    href: require('!file-loader!assets/favicon/manifest.json'),
   }, {
     rel: 'mask-icon',
-    href: require('!file!assets/favicon/safari-pinned-tab.svg'),
+    href: require('!file-loader!assets/favicon/safari-pinned-tab.svg'),
     color: '#aa0078',
   }, {
     rel: 'apple-touch-icon',
     sizes: '180x180',
-    href: require('!file!assets/favicon/apple-touch-icon.png'),
+    href: require('!file-loader!assets/favicon/apple-touch-icon.png'),
   }, {
     rel: 'icon',
     type: 'image/png',
     sizes: '16x16',
-    href: require('!file!assets/favicon/favicon-16x16.png'),
+    href: require('!file-loader!assets/favicon/favicon-16x16.png'),
   }, {
     rel: 'icon',
     type: 'image/png',
     sizes: '32x32',
-    href: require('!file!assets/favicon/favicon-32x32.png'),
+    href: require('!file-loader!assets/favicon/favicon-32x32.png'),
   }],
 };
